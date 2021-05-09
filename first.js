2288bf34a2e04f33184efc2bbba4af4fac91b10a
Bruno Dulcetti <brunodulcetti@gmail.com>
2021-05-09 17:07:00 -0300

    feat: add console and forst method

--- first.js
+++ first.js
@@ -1,3 +1,10 @@
 ((win, doc) => {
   console.info("Olar, vem sempre aqui?");
+  console.info("Mais um console");
+
+  function myMethod() {
+    return "Tá tudo bem";
+  }
+
+  myMethod();
 })(window, document);
